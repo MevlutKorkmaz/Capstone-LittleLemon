1c873bee1057f793007d6c494ac725dd7c610d4c
MevlutKorkmaz <korkmazme@mef.edu.tr>
2024-08-07 14:21:55 +0300

    adding styles

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div>
-
+    <div className="app-container">
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
