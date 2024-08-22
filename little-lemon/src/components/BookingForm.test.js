97f95a4b1461f2d516b2b93bb8638be74a8270f0
MevlutKorkmaz <korkmazme@mef.edu.tr>
2024-08-22 14:39:57 +0300

    tests for booking form

--- little-lemon/src/components/BookingForm.test.js
+++ little-lemon/src/components/BookingForm.test.js
@@ -1,11 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from '@testing-library/react';
 import BookingForm from './BookingForm';
 
-test('Renders the BookingForm heading', () => {
-  const availableTimes = ['17:00', '18:00', '19:00', '20:00', '21:00'];
-  const dispatch = jest.fn();
+test('Submit button is disabled if required fields are empty', () => {
+  render(<BookingForm availableTimes={[]} dispatch={jest.fn()} submitForm={jest.fn()} />);
 
-  render(<BookingForm availableTimes={availableTimes} dispatch={dispatch} />);
-  const headingElement = screen.getByText("Choose date");
-  expect(headingElement).toBeInTheDocument();
+  const submitButton = screen.getByText(/submit reservation/i);
+  expect(submitButton).toBeDisabled();
+});
+
+test('Submit button is enabled when all fields are valid', () => {
+  render(<BookingForm availableTimes={['17:00', '18:00']} dispatch={jest.fn()} submitForm={jest.fn()} />);
+
+  fireEvent.change(screen.getByLabelText(/choose date/i), { target: { value: '2023-09-01' } });
+  fireEvent.change(screen.getByLabelText(/choose time/i), { target: { value: '17:00' } });
+  fireEvent.change(screen.getByLabelText(/number of guests/i), { target: { value: '2' } });
+  fireEvent.change(screen.getByLabelText(/occasion/i), { target: { value: 'Birthday' } });
+
+  const submitButton = screen.getByText(/submit reservation/i);
+  expect(submitButton).toBeEnabled();
+});
+
+test('Form submits when all fields are valid', () => {
+  const mockSubmitForm = jest.fn();
+
+  render(<BookingForm availableTimes={['17:00', '18:00']} dispatch={jest.fn()} submitForm={mockSubmitForm} />);
+
+  fireEvent.change(screen.getByLabelText(/choose date/i), { target: { value: '2023-09-01' } });
+  fireEvent.change(screen.getByLabelText(/choose time/i), { target: { value: '17:00' } });
+  fireEvent.change(screen.getByLabelText(/number of guests/i), { target: { value: '2' } });
+  fireEvent.change(screen.getByLabelText(/occasion/i), { target: { value: 'Birthday' } });
+
+  fireEvent.click(screen.getByText(/submit reservation/i));
+
+  expect(mockSubmitForm).toHaveBeenCalledTimes(1);
 });
